8715e20c973feb02e336521d5032c6b0f9d333be
TroyaDevelop <goddys777@icloud.com>
2023-03-17 18:26:26 +0300

    Version 0.001

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const rooms = [
         name: "Спальное место",
         description: "На полу накидан стог сена, который уже успел просмердется за долгое время, ведь вряд ли его когда-нибудь меняли, а рядом стоит уже переполненный горшок, заменяющий вам туалет.",
         west: null,
+        locked: false,
     },
 
     {
@@ -12,12 +13,21 @@ const rooms = [
         description: "Сырые стены местами покрыты слоями плесени, освещаемые единственным лучем лунного света который пробивается сквозь дыру в высоком потолке.",
         west: null,
         east: null,
+        locked: false,
     },
 
     {
         name: "У решетки",
         description: "Стойкий запах мерзкой баланды доносится с кухни, аж до вашей камеры. Ни коменданта, ни охраны нет на своих постах, а во всех соседних камерах царит тишина.",
         east: null,
+        locked: false,
+    },
+
+    {
+        name: "У входа в камеру",
+        description: "Факела на стенах едва горят, а коридор выглядит неестественно пустым, без единого патрулирующего стражника.",
+        east: null,
+        locked: true,
     },
     
 ];
@@ -38,43 +48,65 @@ const map = {
     methodsInit(){
         document.querySelector(".north").addEventListener("click", event => {
             if(this.currentRoom.hasOwnProperty('north')){
-                this.currentRoom = this.currentRoom.north;
+                let nextRoom = this.currentRoom.north;
+                if(nextRoom.locked == false){
+                    this.currentRoom = this.currentRoom.north;
                 this.renderRoom();
+                } else {
+                    alert("Заперто!");
+                };
             } else {
                 alert("Тупик!");
-            }
+            };
         });
         document.querySelector(".east").addEventListener("click", event => {
             if(this.currentRoom.hasOwnProperty('east')){
-                this.currentRoom = this.currentRoom.east;
+                let nextRoom = this.currentRoom.east;
+                if(nextRoom.locked == false){
+                    this.currentRoom = this.currentRoom.east;
                 this.renderRoom();
+                } else {
+                    alert("Заперто!");
+                };
             } else {
                 alert("Тупик!");
-            }
+            };
         });
         document.querySelector(".south").addEventListener("click", event => {
             if(this.currentRoom.hasOwnProperty('south')){
-                this.currentRoom = this.currentRoom.south;
+                let nextRoom = this.currentRoom.south;
+                if(nextRoom.locked == false){
+                    this.currentRoom = this.currentRoom.south;
                 this.renderRoom();
+                } else {
+                    alert("Заперто!");
+                };
             } else {
                 alert("Тупик!");
-            }
+            };
         });
         document.querySelector(".west").addEventListener("click", event => {
             if(this.currentRoom.hasOwnProperty('west')){
-                this.currentRoom = this.currentRoom.west;
+                let nextRoom = this.currentRoom.west;
+                if(nextRoom.locked == false){
+                    this.currentRoom = this.currentRoom.west;
                 this.renderRoom();
+                } else {
+                    alert("Заперто!");
+                };
             } else {
                 alert("Тупик!");
-            }
+            };
         });
     },
 
     roomsInit(){
+        //Тюремная камера
         this.rooms[0].west = this.rooms[1];
         this.rooms[1].east = this.rooms[0];
         this.rooms[1].west = this.rooms[2];
         this.rooms[2].east = this.rooms[1];
+        this.rooms[2].west = this.rooms[3];
     },
     
 
